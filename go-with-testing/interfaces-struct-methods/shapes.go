d2e7d5cea8c04334bb691e3882792a63fcee80e6
Shivansh Charak <shivansh17charak@gmail.com>
2025-08-22 06:01:57 +0530

    array-and0slices

---
+++ go-with-testing/interfaces-struct-methods/shapes.go
@@ -0,0 +1,21 @@
+package main
+
+import "math"
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+type Circle struct {
+	radius float64
+}
+type Shape interface {
+	Area() float64
+}
+
+func (r Rectangle) Area() float64 {
+	return (r.width * r.height)
+}
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
